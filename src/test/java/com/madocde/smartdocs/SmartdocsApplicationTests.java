b6f175392fb7e871e4a5cacc0c97946fae6b468c
abhishekrajmad <madaramabhishekraj@gmail.com>
2026-08-28 21:06:06 +0530

    feat: init springboot project with pgvector extension and docker-compose

---
+++ src/test/java/com/madocde/smartdocs/SmartdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madocde.smartdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
